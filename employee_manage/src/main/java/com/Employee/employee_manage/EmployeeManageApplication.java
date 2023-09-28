7c8b5c480e614c891571b6125944ec8ca2f0b019
premkumarg-7 <133476191+premkumarg-7@users.noreply.github.com>
2023-09-28 08:37:12 +0530

    Add files via upload

---
+++ employee_manage/src/main/java/com/Employee/employee_manage/EmployeeManageApplication.java
@@ -0,0 +1,13 @@
+package com.Employee.employee_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManageApplication.class, args);
+	}
+
+}
---
+++ employee_manage/src/test/java/com/Employee/employee_manage/EmployeeManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Employee.employee_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
